752af722e78848155202b6924f15abcd901e2c64
pedroveloper <pedrom_t@hotmail.com>
2020-12-18 02:18:02 +0100

    Login Design

---
+++ app/src/main/java/com/pedrodev/pedrosolutions/view/CrearCuentaActivity.java
@@ -0,0 +1,17 @@
+package com.pedrodev.pedrosolutions.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pedrodev.pedrosolutions.R;
+
+public class CrearCuentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+    }
+
+}
